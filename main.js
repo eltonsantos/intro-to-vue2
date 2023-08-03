5fdf378292170ce3e6b19ecc5a2cb228070aefb2
Elton Santos <eltonaxl@hotmail.com>
2023-08-03 01:38:10 -0300

    Event and methods

--- main.js
+++ main.js
@@ -4,6 +4,28 @@ var app = new Vue({
     product: 'Testxxxxe',
     image: 'image.jpg',
     inventory: 11,
-    inStock: true
+    inStock: true,
+    details: ['80%', '20%', 'neutral'],
+    variants: [
+      {
+        variantId: 1,
+        variantColor: "green",
+        variantImage: "image.jpg"
+      },
+      {
+        variantId: 2,
+        variantColor: "blue",
+        variantImage: "image2.jpg"
+      },
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    }
   }
 })
