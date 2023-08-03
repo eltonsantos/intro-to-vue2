229b7714b1b5b37c38ec12c2b8d46dda88ea1cc0
Elton Santos <eltonaxl@hotmail.com>
2023-08-03 01:57:37 -0300

    Computed properties

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Testxxxxe',
-    image: 'image.jpg',
+    product: 'Socks',
+    brand: 'Vue Mastery',
+    selectedVariant: 0,
     inventory: 11,
-    inStock: true,
     details: ['80%', '20%', 'neutral'],
     variants: [
       {
         variantId: 1,
         variantColor: "green",
-        variantImage: "image.jpg"
+        variantImage: "image.jpg",
+        variantQuantity: 10
       },
       {
         variantId: 2,
         variantColor: "blue",
-        variantImage: "image2.jpg"
+        variantImage: "image2.jpg",
+        variantQuantity: 0
       },
     ],
     cart: 0
@@ -24,8 +26,20 @@ var app = new Vue({
     addToCart() {
       this.cart += 1
     },
-    updateProduct(variantImage) {
-      this.image = variantImage
+    updateProduct(index) {
+      this.selectedVariant = index
+      console.log(index)
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
     }
   }
 })
